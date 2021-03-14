b5e3b5a1666db5116484754f6ac6e4fe7271494d
muhammedulas <muhammedulas3333@gmail.com>
2021-03-14 23:12:42 +0300

    .

--- src/app/services/loginService.service.ts
+++ src/app/services/loginService.service.ts
@@ -2,32 +2,17 @@ import { Injectable } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser/platform-browser';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { tokenResp } from '../models/tokenResp';
+import { Observable, observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginServiceService {
 
+  public loggedIn: boolean = false;
   private rootUrl = localStorage.getItem('rootUrl');
   private clientID = localStorage.getItem('ClientID');
   private clientSecret = localStorage.getItem('clientSecret');
-  private response: tokenResp = {
-    "access_token": "",
-    "token_type": "",
-    "expires_in": 0,
-    "refresh_token": "",
-    "as:client_id": "",
-    "userName": "",
-    "firmNo": "",
-    "sessionId": "",
-    "dbName": "",
-    "logoDB": "",
-    "isLoginEx": "",
-    "isLogoPlugin": "",
-    "idmToken": "",
-    ".issued": "",
-    ".expires": ""
-  }
 
   constructor(private http: HttpClient) { }
 
@@ -36,6 +21,7 @@ export class LoginServiceService {
     let headers = new HttpHeaders().set('Authorization', 'Basic').set('Content-Type', 'application/json').set('Accept', 'application/json');
     const body: string = "grant_type=password&username=" + usr + "&firmno=" + frmNo + "&password=" + pw
     return this.http.post<tokenResp>('http://localhost:33082/api/v1/token', body, { headers });
+
   }
 
 }
